0267dea654beb9d510e58bbebdd9291fddca2a81
caseynshan <ccupp@petroweb.com>
2015-05-04 15:07:03 -0600

    build solution

---
+++ build/build.config.js
@@ -0,0 +1,9 @@
+({	
+	name:"main",
+	baseUrl:"../js",
+	mainConfigFile:"../js/main.js",
+	out:"../js/main-optimized.min.js",
+	optimize:"none",
+	generateSourceMaps:true,
+	preserveLicenseComments:false
+})
